249f35acb2984fd6926eee47c5d0b428a227ca67
oyke <oyke@foxmail.com>
2020-01-01 07:17:53 +0800

    项目初始化

---
+++ app/src/main/java/com/oyke/wanandroid/MainActivity.java
@@ -0,0 +1,14 @@
+package com.oyke.wanandroid;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
